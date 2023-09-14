200992695c4ec0991f0d88fca91e92d1b2f41f80
pedrold10 <pedrogabriel12423@gmail.com>
2023-09-14 09:36:05 -0300

    Gerando construtor, getters e setters para entidade Mensagem

--- src/main/java/com/gugawag/pdist/modelo/Mensagem.java
+++ src/main/java/com/gugawag/pdist/modelo/Mensagem.java
@@ -10,4 +10,22 @@ public class Mensagem {
     private long id;
     private String texto;
 
+    public Mensagem(){
+
+    }
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
 }
